774e695210b7df1c5b7cc373b6531fa16cd364be
August Aldred <hachigatsu14@outlook.com>
2023-08-15 09:39:11 +0100

    setup kata

---
+++ justify-line/__tests__/justify.test.js
@@ -0,0 +1,7 @@
+const { justifyText } = require('../justify');
+
+describe('justifyText()', () => { 
+    test('if passed one word it returns the correct number of spaces', () => {
+
+    })
+ })
---
+++ justify-line/justify.js
@@ -0,0 +1,3 @@
+module.exports.justifyText = (str, maxLength) => {
+
+}
