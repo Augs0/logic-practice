c8f273229245d87627f9854471d344b7d1d08615
August Aldred <hachigatsu14@outlook.com>
2023-09-11 11:06:35 +0100

    add justify kata solution

--- justify-line/__tests__/justify.test.js
+++ justify-line/__tests__/justify.test.js
@@ -2,6 +2,17 @@ const { justifyText } = require('../justify');
 
 describe('justifyText()', () => { 
     test('if passed one word it returns the correct number of spaces', () => {
-
-    })
+        expect(justifyText('august', 8)).toBe('  august');
+        expect(justifyText('egg',10)).toBe('       egg');
+        expect(justifyText('planet', 6)).toBe('planet');
+    });
+    test('if passed multiple words, returns the string with the correct number of spaces ', () => {
+        expect(justifyText('howdy all',9 )).toBe('howdy all');
+        expect(justifyText('vado in Portogallo', 20)).toBe('vado  in  Portogallo');
+        expect(justifyText('vado in Corea', 16)).toBe('vado   in  Corea');
+        expect(justifyText('egg egg egg egg', 16)).toBe('egg  egg egg egg')
+    });
+    test('should return String exceeds maximum line length if the string is larger than the max length', () => {
+        expect(justifyText('my lovely string', 10) ).toBe('String exceeds maximum line length');
+    });
  })
--- justify-line/justify.js
+++ justify-line/justify.js
@@ -1,3 +1,43 @@
-module.exports.justifyText = (str, maxLength) => {
+function justifyText (str, maxLength) {
+  if(str.length > maxLength){
+    return "String exceeds maximum line length"
+  }
 
+  const numOfSpacesToAdd = maxLength - str.length;
+  let splitStr = str.split(' ');
+
+  if(splitStr.length === 1) {
+    return " ".repeat(numOfSpacesToAdd) + str;
+  }
+  
+  splitStr = splitStr.map((word, index) => {
+    if(index === splitStr.length-1) return word;
+    return word + " ";
+  })
+
+   let counter = 0;
+   let currentWordIndex = 0;
+
+   while(counter < numOfSpacesToAdd) {
+      splitStr[currentWordIndex] = splitStr[currentWordIndex] + " ";
+      counter++;
+      currentWordIndex++
+      if(currentWordIndex === splitStr.length - 1){
+        currentWordIndex = 0;
+      }
+  }
+
+  let result = '';
+  
+  for (let i = 0; i < splitStr.length; i++) {
+    const word = splitStr[i];
+    for (let j = 0; j < word.length; j++) {
+      const letter = word[j];
+      result += letter;
+    }
+    
+  }
+  return result;
 }
+
+module.exports = { justifyText }
